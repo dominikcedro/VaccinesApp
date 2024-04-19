fbbf46da568c5f101253ff4eb7b274c046ae8198
mwdziak <mwdziak@gmail.com>
2024-04-19 09:30:20 +0200

    Dose and Vaccine data classes defined.

---
+++ app/src/main/java/com/example/vaccineapp/domain/Dose.kt
@@ -0,0 +1,10 @@
+package com.example.vaccineapp.domain
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Dose(
+    val id: Long,
+    val doseNumber: Int,
+    val monthsAfterPreviousDose: Int
+)
---
+++ app/src/main/java/com/example/vaccineapp/domain/Vaccine.kt
@@ -0,0 +1,12 @@
+package com.example.vaccineapp.domain
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Vaccine(
+    val id: Long,
+    val name: String,
+    val recommendedAgeMonthsLowerBound: Int,
+    val recommendedAgeMonthsUpperBound: Int,
+    val doses: List<Dose>
+)
