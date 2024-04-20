89343bc570d9efeda2a4c64c0a398afbc0f5f089
mwdziak <mwdziak@gmail.com>
2024-04-20 18:21:54 +0200

    Updated ec2 ip

--- app/src/main/java/com/example/vaccineapp/service/HttpService.kt
+++ app/src/main/java/com/example/vaccineapp/service/HttpService.kt
@@ -14,7 +14,7 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 
 class HttpService(private val noAuthHttpClient: HttpClient, private val defaultHttpClient: HttpClient) {
-    private val usersServiceUrl = "http://ec2-13-60-51-62.eu-north-1.compute.amazonaws.com"
+    private val usersServiceUrl = "http://ec2-13-49-243-234.eu-north-1.compute.amazonaws.com"
 
     suspend fun authenticate(authenticationRequest: AuthenticationRequest): AuthenticationResponse {
         val url = "$usersServiceUrl/auth/login"
