64768dbc90fc9a82ef4f4b9db1a96648cf105606
mwdziak <mwdziak@gmail.com>
2024-04-21 16:47:43 +0200

    Firebase service updated to handle notifications in the foreground

--- app/src/main/java/com/example/vaccineapp/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/vaccineapp/service/MyFirebaseMessagingService.kt
@@ -1,13 +1,47 @@
 package com.example.vaccineapp.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.vaccineapp.R
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
-
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+
+        if (remoteMessage.notification != null) {
+            showNotification(remoteMessage.notification)
+        }
+    }
+
+    private fun showNotification(notification: RemoteMessage.Notification?) {
+        val channelId = "channel_id"
+        val channelName = "channel_name"
+        val notificationId = 0
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setContentTitle(notification?.title)
+            .setContentText(notification?.body)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(notificationId, notificationBuilder.build())
     }
 }
