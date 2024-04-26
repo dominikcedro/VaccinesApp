fb821b6be63e0dfc869c55af02fe19bc845fbcd4
Domin C <dominikcedro00@gmail.com>
2024-04-26 19:05:38 +0200

    UPDATE now administered vaccines can be added, theres still an issue with adding scheduled vaccinations

--- app/src/main/java/com/example/vaccineapp/viewmodel/AddAdministeredVaccinationViewModel.kt
+++ app/src/main/java/com/example/vaccineapp/viewmodel/AddAdministeredVaccinationViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.vaccineapp.viewmodel
 
 import HttpService
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -9,13 +11,19 @@ import com.example.vaccineapp.domain.AdministeredVaccinationGetRequest
 import com.example.vaccineapp.domain.AdministeredVaccinePostRequest
 import com.example.vaccineapp.domain.Vaccine
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class AddAdministeredVaccinationViewModel(private val httpService: HttpService): ViewModel() {
     private val vaccines = mutableListOf<Vaccine>()
     private val alreadyAdministeredVaccinations = mutableListOf<AdministeredVaccinationGetRequest>()
     private val chosenVaccineIndex = MutableLiveData<Int>()
-    private val chosenTime = MutableLiveData<String>()
-    private val chosenDate = MutableLiveData<String>()
+    private val chosenDate = MutableLiveData<LocalDate>()
+    private val chosenTime = MutableLiveData<LocalTime>()
+
     private val doseNumber = MutableLiveData<Int>()
 
     suspend fun fetchVaccines() {
@@ -36,11 +44,14 @@ class AddAdministeredVaccinationViewModel(private val httpService: HttpService):
         chosenVaccineIndex.value = index
     }
 
-    fun setChosenTime(time: String) {
-        chosenTime.value = time
-    }
+    @RequiresApi(Build.VERSION_CODES.O)
     fun setChosenDate(date: String) {
-        chosenDate.value = date
+        chosenDate.value = LocalDate.parse(date)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setChosenTime(time: String) {
+        chosenTime.value = LocalTime.parse(time)
     }
 
     fun setDoseNumber(number: Int) {
@@ -61,12 +72,20 @@ class AddAdministeredVaccinationViewModel(private val httpService: HttpService):
         return doseNumber.value!! <= vaccines[chosenVaccineIndex.value!!].doses.size
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun postVaccination() {
         viewModelScope.launch {
             val vaccineId = vaccines[chosenVaccineIndex.value!!].id
             val doseNumber = doseNumber.value!!
-            val dateTime = chosenDate.value!! + " " + chosenTime.value!!
-            val administeredVaccine = AdministeredVaccinePostRequest(vaccineId, doseNumber, dateTime)
+
+            // Create a ZonedDateTime object from the chosen date and time
+            val dateTime = LocalDateTime.of(chosenDate.value, chosenTime.value).atZone(ZoneId.systemDefault())
+
+            // Format the ZonedDateTime object to a string
+            val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ")
+            val formattedDateTime = dateTime.format(dateTimeFormatter)
+
+            val administeredVaccine = AdministeredVaccinePostRequest(vaccineId, doseNumber, formattedDateTime)
             httpService.addAdministeredVaccine(administeredVaccine)
         }
     }
--- app/src/main/java/com/example/vaccineapp/viewmodel/AddScheduledVaccinationViewModel.kt
+++ app/src/main/java/com/example/vaccineapp/viewmodel/AddScheduledVaccinationViewModel.kt
@@ -1,24 +1,33 @@
 package com.example.vaccineapp.viewmodel
 
 import HttpService
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.vaccineapp.domain.AdministeredVaccinationGetRequest
+import com.example.vaccineapp.domain.Reminder
 import com.example.vaccineapp.domain.ReminderPostRequest
 import com.example.vaccineapp.domain.ScheduledVaccinationGetRequest
+import com.example.vaccineapp.domain.ScheduledVaccinationPostRequest
 import com.example.vaccineapp.domain.Vaccine
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class AddScheduledVaccinationViewModel(private val httpService: HttpService) : ViewModel() {
-    private var headerText = "Schedule first dose"
-    private val reminders = mutableListOf<ReminderPostRequest>()
+    public var headerText = MutableLiveData<String>("Schedule dose number 1")
+    private val reminders = mutableListOf<Reminder>()
     private val recommendedVaccines = mutableListOf<Vaccine>()
-    private val vaccines = mutableListOf<Vaccine>()
     private val alreadyScheduledVaccinations = mutableListOf<ScheduledVaccinationGetRequest>()
     private val chosenVaccineIndex = MutableLiveData<Int>()
-    private val chosenTime = MutableLiveData<String>()
-    private val chosenDate = MutableLiveData<String>()
+    private var doseNumber = MutableLiveData<Int>(1)
+    private val chosenZonedDateTime = MutableLiveData<ZonedDateTime?>()
+    private var previousDoseZonedDateTime: ZonedDateTime? = null
 
-    suspend fun fetchrecommendedVaccines() {
+    suspend fun fetchRecommendedVaccines() {
         recommendedVaccines.clear()
         recommendedVaccines.addAll(httpService.fetchRecommendedVaccines())
     }
@@ -29,21 +38,74 @@ class AddScheduledVaccinationViewModel(private val httpService: HttpService) : V
     }
 
     fun getVaccineNames(): List<String> {
-        return vaccines.map { it.name }
+
+        return recommendedVaccines.map { it.name }
     }
 
     fun setChosenVaccineIndex(index: Int) {
         chosenVaccineIndex.value = index
     }
 
-    fun setChosenTime(time: String) {
-        chosenTime.value = time
+    fun setChosenZonedDateTime(zonedDateTime: ZonedDateTime) {
+        chosenZonedDateTime.value = zonedDateTime
     }
-    fun setChosenDate(date: String) {
-        chosenDate.value = date
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setChosenDate(date: LocalDate) {
+        val time = chosenZonedDateTime.value?.toLocalTime() ?: LocalTime.MIDNIGHT
+        chosenZonedDateTime.value = ZonedDateTime.of(LocalDateTime.of(date, time), chosenZonedDateTime.value?.zone ?: ZoneId.systemDefault())
     }
 
-    fun addReminder(reminder: ReminderPostRequest) {
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setChosenTime(time: LocalTime) {
+        val date = chosenZonedDateTime.value?.toLocalDate() ?: LocalDate.now()
+        chosenZonedDateTime.value = ZonedDateTime.of(LocalDateTime.of(date, time), chosenZonedDateTime.value?.zone ?: ZoneId.systemDefault())
+    }
+
+    fun addReminder(reminder: Reminder) {
         this.reminders.add(reminder)
+    }
+
+    fun removeReminder(reminder: Reminder) {
+        this.reminders.remove(reminder)
+    }
+
+    fun addNextDose(){
+        if (!isThereNextDose()) {
+            return
+        }
+        doseNumber.value = doseNumber.value!! + 1
+        headerText.value = "Schedule dose number ${doseNumber.value}"
+        reminders.clear()
+        previousDoseZonedDateTime = chosenZonedDateTime.value
+        chosenZonedDateTime.value = null
+    }
+    fun isThereNextDose(): Boolean {
+        return doseNumber.value!! < recommendedVaccines[chosenVaccineIndex.value!!].doses.size
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getRecommendedDateForDose(): ZonedDateTime? {
+        val months = recommendedVaccines[chosenVaccineIndex.value!!].doses[doseNumber.value!!-1].monthsAfterPreviousDose
+        return months?.toLong()?.let { previousDoseZonedDateTime!!.plusMonths(it) }
+    }
+
+    fun getCurrentDoseNumber (): Int {
+        return doseNumber.value!!
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    suspend fun postScheduledVaccination() {
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ")
+        val reminderPostRequests = mutableListOf<ReminderPostRequest>()
+        reminders.forEach {
+            reminderPostRequests.add(ReminderPostRequest(it.dateTime!!.format(dateTimeFormatter)))
+        }
+
+        val vaccineId = recommendedVaccines[chosenVaccineIndex.value!!].id
+        val doseNumber = doseNumber.value!!
+        val dateTime = chosenZonedDateTime.value!!
+        val scheduledVaccine = ScheduledVaccinationPostRequest(vaccineId, doseNumber, dateTime.format(dateTimeFormatter), reminderPostRequests)
+
+        httpService.postScheduledVaccination(scheduledVaccine)
     }
 }
