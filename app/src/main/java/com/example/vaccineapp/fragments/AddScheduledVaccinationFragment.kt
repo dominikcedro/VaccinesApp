118c1d0eb19531b21507c5e4d05b5daf07cbbecb
Domin C <dominikcedro00@gmail.com>
2024-04-29 19:16:36 +0200

    Reverted back the changes in add scheduled vaccination freagment.

--- app/src/main/java/com/example/vaccineapp/fragments/AddScheduledVaccinationFragment.kt
+++ app/src/main/java/com/example/vaccineapp/fragments/AddScheduledVaccinationFragment.kt
@@ -3,6 +3,7 @@ package com.example.vaccineapp.fragments
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,19 +13,25 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import com.example.vaccineapp.R
-import com.example.vaccineapp.databinding.FragmentAddAdministeredVaccineBinding
 import com.example.vaccineapp.databinding.FragmentAddScheduledVaccinationBinding
+import com.example.vaccineapp.domain.Reminder
 import com.example.vaccineapp.domain.ReminderPostRequest
 import com.example.vaccineapp.viewmodel.AddScheduledVaccinationViewModel
+import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.time.Instant
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.OffsetDateTime
+import java.time.Period
 import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class AddScheduledVaccinationFragment : Fragment() {
     private val viewModel: AddScheduledVaccinationViewModel by viewModel()
@@ -55,83 +62,138 @@ class AddScheduledVaccinationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         lifecycleScope.launch {
             viewModel.fetchRecommendedVaccines()
-            viewModel.fetchScheduledVaccinations()
             initDropdownMenu()
+            viewModel.fetchScheduledVaccinations()
+
         }
 
         binding.chooseVaccineTextView.setOnItemClickListener { _, _, position, _ ->
             viewModel.setChosenVaccineIndex(position)
         }
 
-
-        binding.pickDate.setOnClickListener {
-            val datePicker = MaterialDatePicker.Builder.datePicker().build()
-            datePicker.addOnPositiveButtonClickListener { selectedDate ->
-                val date = Instant.ofEpochMilli(selectedDate).atZone(ZoneId.systemDefault()).toLocalDate()
-                val dateString = date.toString()
-                viewModel.setChosenDate(dateString)
-                binding.pickDate.setText(dateString)
-            }
-            datePicker.show(childFragmentManager, "date_picker")
+        viewModel.headerText.observe(viewLifecycleOwner) {
+            binding.tvWelcome.text = it
         }
 
-        binding.pickTime.setOnClickListener {
+
+        binding.pickDate.setOnClickListener {
+            getSelectedDateTime()
+        }
+
+        binding.btnAddReminder.setOnClickListener {
+            val reminder = Reminder()
+            setDateTimeForReminder(reminder)
+        }
+
+
+        binding.btnSubmit.setOnClickListener {
+            if (viewModel.isThereNextDose()) {
+                lifecycleScope.launch {
+                    viewModel.postScheduledVaccination()
+                    viewModel.addNextDose()
+                    binding.pickDate.setText("")
+                    binding.reminderLayout.removeAllViews()
+                    binding.chooseVaccineTextView.isEnabled = false
+                }
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getSelectedDateTime(){
+        val datePickerBuilder = MaterialDatePicker.Builder.datePicker()
+
+
+
+        if (viewModel.getCurrentDoseNumber() > 1){
+            val oneWeek = Period.ofWeeks(1)
+            val recommendedDate = viewModel.getRecommendedDateForDose()
+
+            val startDate = recommendedDate!!.minus(oneWeek).toInstant().toEpochMilli()
+            val endDate = recommendedDate.plus(oneWeek).toInstant().toEpochMilli()
+
+            val constraintsBuilder = CalendarConstraints.Builder()
+                .setStart(startDate)
+                .setEnd(endDate)
+
+            val constraints = constraintsBuilder.build()
+            datePickerBuilder.setCalendarConstraints(constraints)
+
+        }
+        val datePicker = datePickerBuilder.build()
+
+        datePicker.addOnPositiveButtonClickListener { selectedDate ->
+            val date = Instant.ofEpochMilli(selectedDate).atZone(ZoneId.systemDefault()).toLocalDate()
+
+
             val timePicker = MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_24H).build()
             timePicker.addOnPositiveButtonClickListener {
-                val hour = String.format("%02d", timePicker.hour)
-                val minute = String.format("%02d", timePicker.minute)
-                viewModel.setChosenTime("$hour:$minute")
-                binding.pickTime.setText("$hour:$minute")
+                val hour = timePicker.hour
+                val minute = timePicker.minute
+                val localTime = LocalTime.of(hour, minute)
+                val localDateTime = LocalDateTime.of(date, localTime)
+
+                val zoneOffset = OffsetDateTime.now(ZoneId.systemDefault()).offset
+                val zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.of(zoneOffset.id))
+
+                viewModel.setChosenZonedDateTime(zonedDateTime)
+                val formatter = DateTimeFormatter.ofPattern("HH:mm yyyy-MM-dd")
+
+                binding.pickDate.setText(zonedDateTime.format(formatter))
             }
             timePicker.show(childFragmentManager, "time_picker")
         }
-
-
-        binding.btnAddReminder.setOnClickListener {
-            val reminder = ReminderPostRequest()
-            val datePicker = MaterialDatePicker.Builder.datePicker().build()
-            datePicker.addOnPositiveButtonClickListener { selectedDate ->
-                val date = Instant.ofEpochMilli(selectedDate).atZone(ZoneId.systemDefault()).toLocalDate()
-                val dateString = date.toString()
-                reminder.dateTime = dateString
-
-                val timePicker = MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_24H).build()
-                timePicker.addOnPositiveButtonClickListener {
-                    val hour = String.format("%02d", timePicker.hour)
-                    val minute = String.format("%02d", timePicker.minute)
-                    reminder.dateTime += "T$hour:$minute"
-
-                    viewModel.addReminder(reminder)
-                    addReminder(reminder.dateTime)
-                }
-                timePicker.show(childFragmentManager, "time_picker")
-            }
-            datePicker.show(childFragmentManager, "date_picker")
-        }
-        binding.btnSubmit.setOnClickListener {
-            viewModel.postVaccination()
-            findNavController().navigate(R.id.action_addScheduledVaccinationFragment_to_ScheduledVaccinationsFragment)
-        }
-
+        datePicker.show(childFragmentManager, "date_picker")
     }
 
-    private fun addReminder(reminderDateTime: String) {
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun setDateTimeForReminder(reminder: Reminder) {
+        val datePicker = MaterialDatePicker.Builder.datePicker().build()
+        datePicker.addOnPositiveButtonClickListener { selectedDate ->
+            val date = Instant.ofEpochMilli(selectedDate).atZone(ZoneId.systemDefault()).toLocalDate()
+
+            val timePicker = MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_24H).build()
+            timePicker.addOnPositiveButtonClickListener {
+                val hour = timePicker.hour
+                val minute = timePicker.minute
+                val localTime = LocalTime.of(hour, minute)
+                val localDateTime = LocalDateTime.of(date, localTime)
+
+                // Get the system's zone offset
+                val zoneOffset = OffsetDateTime.now(ZoneId.systemDefault()).offset
+                val zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.of(zoneOffset.id))
+
+                reminder.dateTime = zonedDateTime
+
+                viewModel.addReminder(reminder)
+                inflateReminder(reminder)
+            }
+            timePicker.show(childFragmentManager, "time_picker")
+        }
+        datePicker.show(childFragmentManager, "date_picker")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun inflateReminder(reminder: Reminder) {
         val newReminder = LayoutInflater.from(requireContext()).inflate(R.layout.item_reminder, binding.reminderLayout, false)
         binding.reminderLayout.addView(newReminder)
-        initReminder(newReminder, reminderDateTime)
+        initReminder(newReminder, reminder)
     }
 
-    private fun initReminder(reminder: View, reminderDateTime: String) {
-        val deleteButton = reminder.findViewById<Button>(R.id.btnDelete)
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun initReminder(reminderView: View, reminder: Reminder) {
+        val deleteButton = reminderView.findViewById<Button>(R.id.btnDelete)
         deleteButton.setOnClickListener {
-            removeReminder(reminder)
+            removeReminder(reminderView, reminder)
         }
-        val reminderDate = reminder.findViewById<TextView>(R.id.tvReminder)
-        reminderDate.text = reminderDateTime
+        val reminderDate = reminderView.findViewById<TextView>(R.id.tvReminder)
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX")
+        reminderDate.text = reminder.dateTime?.format(formatter)
     }
 
-    private fun removeReminder(reminder: View) {
-        binding.reminderLayout.removeView(reminder)
+    private fun removeReminder(reminderView: View, reminder: Reminder) {
+        binding.reminderLayout.removeView(reminderView)
+        viewModel.removeReminder(reminder)
     }
 
     private fun initDropdownMenu() {
--- app/src/main/java/com/example/vaccineapp/viewmodel/AddScheduledVaccinationViewModel.kt
+++ app/src/main/java/com/example/vaccineapp/viewmodel/AddScheduledVaccinationViewModel.kt
@@ -1,32 +1,35 @@
 package com.example.vaccineapp.viewmodel
 
 import HttpService
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.vaccineapp.domain.AdministeredVaccinationGetRequest
+import com.example.vaccineapp.domain.Reminder
 import com.example.vaccineapp.domain.ReminderPostRequest
 import com.example.vaccineapp.domain.ScheduledVaccinationGetRequest
 import com.example.vaccineapp.domain.ScheduledVaccinationPostRequest
 import com.example.vaccineapp.domain.Vaccine
-import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class AddScheduledVaccinationViewModel(private val httpService: HttpService) : ViewModel() {
-    private var headerText = "Schedule first dose"
-    private val reminders = mutableListOf<ReminderPostRequest>()
+    var headerText = MutableLiveData<String>("Schedule dose number 1")
+    private val reminders = mutableListOf<Reminder>()
     private val recommendedVaccines = mutableListOf<Vaccine>()
-    private val vaccines = mutableListOf<Vaccine>()
     private val alreadyScheduledVaccinations = mutableListOf<ScheduledVaccinationGetRequest>()
     private val chosenVaccineIndex = MutableLiveData<Int>()
-    private val chosenTime = MutableLiveData<String>()
-    private val chosenDate = MutableLiveData<String>()
+    private var doseNumber = MutableLiveData<Int>(1)
+    private val chosenZonedDateTime = MutableLiveData<ZonedDateTime?>()
+    private var previousDoseZonedDateTime: ZonedDateTime? = null
 
     suspend fun fetchRecommendedVaccines() {
         recommendedVaccines.clear()
-        val fetchedVaccines = httpService.fetchRecommendedVaccines()
-        recommendedVaccines.addAll(fetchedVaccines)
-        vaccines.clear()
-        vaccines.addAll(fetchedVaccines)
+        recommendedVaccines.addAll(httpService.fetchRecommendedVaccines())
     }
 
     suspend fun fetchScheduledVaccinations() {
@@ -35,31 +38,74 @@ class AddScheduledVaccinationViewModel(private val httpService: HttpService) : V
     }
 
     fun getVaccineNames(): List<String> {
-        return vaccines.map { it.name }
+
+        return recommendedVaccines.map { it.name }
     }
 
     fun setChosenVaccineIndex(index: Int) {
         chosenVaccineIndex.value = index
     }
 
-    fun setChosenTime(time: String) {
-        chosenTime.value = time
+    fun setChosenZonedDateTime(zonedDateTime: ZonedDateTime) {
+        chosenZonedDateTime.value = zonedDateTime
     }
-    fun setChosenDate(date: String) {
-        chosenDate.value = date
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setChosenDate(date: LocalDate) {
+        val time = chosenZonedDateTime.value?.toLocalTime() ?: LocalTime.MIDNIGHT
+        chosenZonedDateTime.value = ZonedDateTime.of(LocalDateTime.of(date, time), chosenZonedDateTime.value?.zone ?: ZoneId.systemDefault())
     }
 
-    fun addReminder(reminder: ReminderPostRequest) {
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setChosenTime(time: LocalTime) {
+        val date = chosenZonedDateTime.value?.toLocalDate() ?: LocalDate.now()
+        chosenZonedDateTime.value = ZonedDateTime.of(LocalDateTime.of(date, time), chosenZonedDateTime.value?.zone ?: ZoneId.systemDefault())
+    }
+
+    fun addReminder(reminder: Reminder) {
         this.reminders.add(reminder)
     }
 
-    fun postVaccination(){
-        viewModelScope.launch{
-            val vaccine = vaccines[chosenVaccineIndex.value!!].id
-            val doseNumber = 1
-            val dateTime = chosenDate.value!! + "T" + chosenTime.value!! + ":00"
-            val scheduledVaccination = ScheduledVaccinationPostRequest(vaccine, doseNumber, dateTime,reminders )
-            httpService.postScheduledVaccination(scheduledVaccination)
+    fun removeReminder(reminder: Reminder) {
+        this.reminders.remove(reminder)
+    }
+
+    fun addNextDose(){
+        if (!isThereNextDose()) {
+            return
         }
+        doseNumber.value = doseNumber.value!! + 1
+        headerText.value = "Schedule dose number ${doseNumber.value}"
+        reminders.clear()
+        previousDoseZonedDateTime = chosenZonedDateTime.value
+        chosenZonedDateTime.value = null
+    }
+    fun isThereNextDose(): Boolean {
+        return doseNumber.value!! < recommendedVaccines[chosenVaccineIndex.value!!].doses.size
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getRecommendedDateForDose(): ZonedDateTime? {
+        val months = recommendedVaccines[chosenVaccineIndex.value!!].doses[doseNumber.value!!-1].monthsAfterPreviousDose
+        return months?.toLong()?.let { previousDoseZonedDateTime!!.plusMonths(it) }
+    }
+
+    fun getCurrentDoseNumber (): Int {
+        return doseNumber.value!!
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    suspend fun postScheduledVaccination() {
+        val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ")
+        val reminderPostRequests = mutableListOf<ReminderPostRequest>()
+        reminders.forEach {
+            reminderPostRequests.add(ReminderPostRequest(it.dateTime!!.format(dateTimeFormatter)))
+        }
+
+        val vaccineId = recommendedVaccines[chosenVaccineIndex.value!!].id
+        val doseNumber = doseNumber.value!!
+        val dateTime = chosenZonedDateTime.value!!
+        val scheduledVaccine = ScheduledVaccinationPostRequest(vaccineId, doseNumber, dateTime.format(dateTimeFormatter), reminderPostRequests)
+
+        httpService.postScheduledVaccination(scheduledVaccine)
     }
 }
