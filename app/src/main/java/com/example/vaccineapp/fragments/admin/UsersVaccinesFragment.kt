7378eea2293b277d22ff9109a75c7ab7dc5f402f
mwdziak <mwdziak@gmail.com>
2024-05-10 11:08:58 +0200

    Formatted the date in last RV

--- app/src/main/java/com/example/vaccineapp/fragments/admin/UsersVaccinesFragment.kt
+++ app/src/main/java/com/example/vaccineapp/fragments/admin/UsersVaccinesFragment.kt
@@ -63,4 +63,6 @@ class UsersVaccinesFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
--- app/src/main/java/com/example/vaccineapp/users_vaccines_recycler_view/UsersScheduledVaccinationAdapter.kt
+++ app/src/main/java/com/example/vaccineapp/users_vaccines_recycler_view/UsersScheduledVaccinationAdapter.kt
@@ -1,12 +1,18 @@
 package com.example.vaccineapp.users_vaccines_recycler_view
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.example.vaccineapp.R
 import com.example.vaccineapp.domain.ScheduledVaccinationGetRequest
 import com.example.vaccineapp.domain.Vaccine
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
 class UsersScheduledVaccinationAdapter(
     private val vaccines: List<ScheduledVaccinationGetRequest>,
     private val onEditClick: (Long) -> Unit  // Change this line
@@ -17,13 +23,23 @@ class UsersScheduledVaccinationAdapter(
         return UsersScheduledViewHolder(view)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: UsersScheduledViewHolder, position: Int) {
         val vaccine = vaccines[position]
         holder.vaccineName.text = vaccines[position].vaccine.name
-        holder.vaccineDate.text = vaccine.dateTime
+        holder.vaccineDate.text = formatDate(vaccine.dateTime)
         holder.editButton.setOnClickListener { onEditClick(vaccine.id) }  // This line is now correct
     }
 
     override fun getItemCount() = vaccines.size
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun formatDate(date: String): String {
+        val targetFormatter = DateTimeFormatter.ofPattern("HH:mm yyyy-MM-dd")
+        val sourceFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ")
+        val zonedDate = ZonedDateTime.parse(date, sourceFormatter)
+        val zonedDateWithSystemOffset = zonedDate.withZoneSameInstant(ZoneId.systemDefault())
+        return zonedDateWithSystemOffset.format(targetFormatter)
+    }
 
 }
